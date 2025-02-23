55cd54110f514b662fe8c5f397c9ab2edd141e14
unknown <clyde.gerance@student.ateneo.edu>
2025-02-23 23:26:18 +0800

    created assembly files

---
+++ Assembly/cppAssembly2.cpp
@@ -0,0 +1,9 @@
+//cppAssembly2.cpp
+int dummy(int a, int b){
+    int x = a + b;
+    x = a - b;
+    x = a * b;
+    x = a / b;
+    x = a % b;
+    return 0;
+}
---
+++ Assembly/main.cpp
@@ -0,0 +1,5 @@
+// still in cppAssembly2.cpp, after dummy( )
+int main(void){
+    int temp = dummy(19,3);
+    return 0;
+}
